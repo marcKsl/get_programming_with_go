c13c40a87eff2fea0de108b10f82043dec153861
marcKsl <m.kassel148@gmail.com>
2024-02-10 13:50:54 +0100

    2.4

--- main.go
+++ main.go
@@ -5,10 +5,21 @@ import (
 )
 
 func main() {
-	const hours = 24
-	var speed = 100800
-	var distance = 96300000
 
-	fmt.Println(distance/speed/hours, "tage")
+	// 2.4.1
 
+	var a, b = 10, 20
+
+	// 2.4.2
+
+	b += 1
+	a /= 5
+	b += a
+	a++
+	a--
+	fmt.Println(a, b)
+
+	var weight = 90
+	weight -= 2
+	fmt.Println("Shortest way to substract 2 from a variable -> 90 -= 2 ->", weight)
 }
