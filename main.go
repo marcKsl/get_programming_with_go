48cc55617075da6f0b9f73639fd60371f95fa165
marcKsl <m.kassel148@gmail.com>
2024-02-11 15:13:09 +0100

    3.5

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	const year = 2400
-	fmt.Println((year%4 == 0 && year%100 != 0) || year%400 == 0)
 
+	// Two ways to implement switch cases, also note the use of fallthrough and default
+
+	// Way number one switch over a variable:
+
+	const floor = 2
+
+	switch floor {
+	case 1:
+		fmt.Println("This is the first floor.")
+	case 2:
+		fmt.Println("This is the ground floor.")
+		fallthrough
+	case 3:
+		fmt.Println("This is the basement and the ceiling seems kinda bad")
+	default:
+		fmt.Println("Still outside!")
+	}
+
+	const parallelDimensionFloor = 1
+
+	switch parallelDimensionFloor {
+	case 1:
+		fmt.Println("This is the first floor.")
+	case 2:
+		fmt.Println("This is the ground floor.")
+		fallthrough
+	case 3:
+		fmt.Println("This is the basement and the ceiling seems kinda bad")
+	default:
+		fmt.Println("Still outside?")
+	}
 }
