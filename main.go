88900b570d5a636edebc25071f218ef6d978adb2
marcKsl <m.kassel148@gmail.com>
2024-02-14 22:29:27 +0100

    Lesson 5 Capstone

--- main.go
+++ main.go
@@ -3,43 +3,51 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
-var era = "AD"
-
-func isLeap(n int) bool {
-	return (n%4 == 0 && n%100 != 0) || n%400 == 0
-}
-
-func getRandomDate() (int, int, int) {
-
-	year := rand.Intn(2078)
-	month := rand.Intn(12) + 1
-	daysInMonth := 31
-
-	switch month {
-	case 2:
-		if isLeap(year) {
-			daysInMonth = 29
-		} else {
-
-			daysInMonth = 28
-		}
-	case 4, 6, 9, 11:
-		daysInMonth = 30
-	}
-
-	day := rand.Intn(daysInMonth) + 1
-	return year, month, day
-}
-
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Printf("Versuch Nummer %v: \n", i+1)
-		year, month, day := getRandomDate()
-		fmt.Println(era, year, month, day)
-		fmt.Printf("------------------\n")
-		time.Sleep(400 * time.Millisecond)
+
+	// for loop needs to print every time all of the four attributes, so
+	fmt.Printf("%-15v%-25v%-15v%-15v$ %-15v\n", 1, "Spaceline", "Days", "Trip type", "Price")
+	fmt.Println("2 =============================================================================")
+	for i := 3; i < 13; i++ {
+		fmt.Printf("%-15v%-25v%-15v%-15v$ %-15v\n", i, getSpaceCompany(), getTraveltime(), getTripType(), getPrice())
 	}
+
+}
+
+func getSpaceCompany() string {
+	choiceOne := "Virgin Galaxy"
+	choiceTwo := "SpaceX"
+	choiceThree := "Space Adventures"
+
+	switch random := rand.Intn(3); random {
+	case 0:
+		return choiceOne
+	case 1:
+		return choiceTwo
+	default:
+		return choiceThree
+	}
+}
+
+func getTraveltime() int {
+	// get Traveltime to Mars at October 13th, 2020 in days
+	return 62100000 / (rand.Intn(15) + 16) / 60 / 24
+}
+
+func getTripType() string {
+	choiceOne := "Round-trip"
+	choiceTwo := "One-way"
+
+	switch random := rand.Intn(3); random {
+	case 0:
+		return choiceOne
+	default:
+		return choiceTwo
+	}
+}
+
+func getPrice() int {
+	return (rand.Intn(25) + 36) * 1000000
 }
