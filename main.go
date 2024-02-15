d018952a1e967e8ef06fd9c50812fb55cdab228b
marcKsl <m.kassel148@gmail.com>
2024-02-15 01:13:10 +0100

    Lesson 6 real numbers

--- main.go
+++ main.go
@@ -5,49 +5,24 @@ import (
 	"math/rand"
 )
 
+func getRandomCoin() float64 {
+	switch i := rand.Intn(3); i {
+	case 0:
+		return 0.05
+	case 1:
+		return 0.10
+	default:
+		return 0.25
+	}
+}
+
 func main() {
 
-	// for loop needs to print every time all of the four attributes, so
-	fmt.Printf("%-15v%-25v%-15v%-15v$ %-15v\n", 1, "Spaceline", "Days", "Trip type", "Price")
-	fmt.Println("2 =============================================================================")
-	for i := 3; i < 13; i++ {
-		fmt.Printf("%-15v%-25v%-15v%-15v$ %-15v\n", i, getSpaceCompany(), getTraveltime(), getTripType(), getPrice())
+	piggyBank := 0.0
+
+	for i := 0; piggyBank <= 20; i++ {
+		piggyBank += getRandomCoin()
 	}
 
-}
-
-func getSpaceCompany() string {
-	choiceOne := "Virgin Galaxy"
-	choiceTwo := "SpaceX"
-	choiceThree := "Space Adventures"
-
-	switch random := rand.Intn(3); random {
-	case 0:
-		return choiceOne
-	case 1:
-		return choiceTwo
-	default:
-		return choiceThree
-	}
-}
-
-func getTraveltime() int {
-	// get Traveltime to Mars at October 13th, 2020 in days
-	return 62100000 / (rand.Intn(15) + 16) / 60 / 24
-}
-
-func getTripType() string {
-	choiceOne := "Round-trip"
-	choiceTwo := "One-way"
-
-	switch random := rand.Intn(3); random {
-	case 0:
-		return choiceOne
-	default:
-		return choiceTwo
-	}
-}
-
-func getPrice() int {
-	return (rand.Intn(25) + 36) * 1000000
+	fmt.Println(piggyBank)
 }
