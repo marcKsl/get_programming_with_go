5c5005669e0b12eb1b8df228c34d879f09d6455a
marcKsl <m.kassel148@gmail.com>
2024-02-17 23:19:34 +0100

    9.4.1 ROT13 cipher

--- main.go
+++ main.go
@@ -1,36 +1,23 @@
-// now lets go with strings, or what they are actually called: literal string.
-
 package main
 
 import "fmt"
 
 func main() {
+	// ROT13 cipher, just moves every character 13 steps higher.
+	message := "uv vagreangvbany fcnpr fgngvba"
 
-	// all the same, literal values between quotes are inferred to be of the type string.
-	peace := "peace"
-	var war = "war"
-	var defNotWar string = "defNotWar"
+	for i := 0; i < len(message); i++ {
+		c := message[i]
+		// first line generally checks if character is between 'a' and 'z', so no space for example
+		if c >= 'a' && c <= 'z' {
+			c += 13
+			// Handles the overflow in the given 26 character range
+			if c >= 'z' {
+				c -= 26
+			}
 
-	fmt.Println("---------------------------------------------------------------------")
-	fmt.Println(peace, war, defNotWar)
-
-	var blank string // Declaration also initializes with the zero value for its type. The zero value for the string type is "".
-	fmt.Println(blank)
-	fmt.Println("---------------------------------------------------------------------")
-
-	// `` Backticks indicate a raw string literal,e.g. \n wont work in it
-	var rawString string = `Escape Character \nis visible. 
-	Also these badboys can span multiple lines in go. I bet they aren't used for
-	database queries or something like this. Definetly not, no.`
-	var normalString string = "Escape Character \nis not visible and breaks line."
-	fmt.Println(rawString)
-	fmt.Println(normalString)
-
-	// Both, literal and raw strings result in the type string
-
-	fmt.Println("---------------------------------------------------------------------")
-	fmt.Printf("normalString is of type: %T\nAnd has the value:\n%[1]v\n", rawString)
-	fmt.Println("---------------------------------------------------------------------")
-	fmt.Printf("rawString is of type: %T\nAnd has the value:\n%[1]v\n", normalString)
-	fmt.Println("---------------------------------------------------------------------")
+		}
+		// No need to fill a new string / array, just print out every character inline with no space between, genius.
+		fmt.Printf("%c", c)
+	}
 }
