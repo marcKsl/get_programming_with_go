e116ce316079652d4b1b76c3ce768b46651b8cb0
marcKsl <m.kassel148@gmail.com>
2024-02-17 23:48:56 +0100

    9.5 Decoding strings into runes

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import "fmt"
 
+// In Go you need variables of type rune (int32) to get some of the characters inside a UTF-8 String.
+// ASCII digits all fit into a byte (uint8) but the extended UTF-8 Character set contains values, that
+// exceed 256.
+
 func main() {
-	// ROT13 cipher, just moves every character 13 steps higher.
-	message := "uv vagreangvbany fcnpr fgngvba"
-
-	for i := 0; i < len(message); i++ {
-		c := message[i]
-		// first line generally checks if character is between 'a' and 'z', so no space for example
-		if c >= 'a' && c <= 'z' {
-			c += 13
-			// Handles the overflow in the given 26 character range
-			if c >= 'z' {
-				c -= 26
-			}
-
-		}
-		// No need to fill a new string / array, just print out every character inline with no space between, genius.
-		fmt.Printf("%c", c)
+	s := "Hello, 世界"
+	// If used with a string, the range keyword returns two values
+	// The index in bytes (can be used as decimal value aswell) at the current position in the loop
+	// And the rune value at that specific position (rember runes are an alias to int32)
+	fmt.Println("----------------------------------------------------------------")
+	fmt.Println("---Using runes to access single characters inside strings-------")
+	fmt.Println("----------------------------------------------------------------")
+	for index, runeValue := range s {
+		fmt.Printf("%-3d=%2c\n", index, runeValue)
 	}
+	fmt.Println("----------------------------------------------------------------")
+	fmt.Println("---you can leave the index, and just use a _ blank identifier---")
+	fmt.Println("----------------------------------------------------------------")
+	for _, runeValue := range s {
+		fmt.Printf("%c\n", runeValue)
+	}
+	fmt.Println("----------------------------------------------------------------")
+	fmt.Println("---This only works with the range keyword because it also------")
+	fmt.Println("---returns the unicode code point, that is of type rune.--------")
+	fmt.Println("----------------------------------------------------------------")
 }
