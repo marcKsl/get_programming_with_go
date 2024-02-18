cfdb83d3547fc3ba17f1e27e40f61f3eb8f024cd
marcKsl <m.kassel148@gmail.com>
2024-02-18 21:45:22 +0100

    Lesson 10.3 convert types with caution

--- main.go
+++ main.go
@@ -2,43 +2,37 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math"
 )
 
 func main() {
-
-	// Use ROT13 to cipher a message that contains unicode characters, and thus runes(int32)
-
-	message := "Hola Estación Espacial Internacional"
-	fmt.Println("")
-	fmt.Println("")
-	for _, r := range message {
-		c := r
-		if c >= 'a' && c <= 'z' { // Lowercase
-			c = c + 13
-			if c > 'z' {
-				c = c - 26
-			}
-		} else if c >= 'A' && c <= 'Z' { // Uppercase
-			c = c + 13
-			if c > 'Z' {
-				c = c - 26
-			}
-		}
-		fmt.Printf("%c", c)
+	// The following int16 will overflow
+	// if the if clause would not catch exceeding values,
+	// causing it to wrap around and print:
+	// -32768. The lowest possible number,
+	// respectively the next possible number.
+	// This is standard behaviour in go,
+	// as it always tends to wrap around in such situations
+	var bh float64 = 32768
+	// The following constant values of math package are untyped.
+	// As discussed in Lesson 8, constants in go are present at compile
+	// time, thus they can also be untyped.
+	// They infer the type out of the situation they are used in
+	// Constants can also be typed.
+	// That's why we can compare a float64 value to the constants directly:
+	if bh < math.MinInt16 || bh > math.MaxInt16 {
+		fmt.Println("Convertion not possible, number would overflow the type int16.")
+		fmt.Println("Stay within the limits: Lowest:", math.MinInt16, ", highest:", math.MaxInt16)
+	} else {
+		var h = int16(bh)
+		fmt.Println(h)
 	}
-
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Print("Translating")
-	for i := 0; i < 10; i++ {
-		fmt.Print(".")
-		time.Sleep(150 * time.Millisecond)
+	// Question: What code will determine if the variable v
+	// is within the range of an 8-bit unsigned integer?
+	v := 255
+	if v <= math.MaxUint8 && v >= 0 {
+		fmt.Println(v, "is in range of an 8-bit unsigned integer.")
+	} else {
+		fmt.Println(v, "is not in range of an 8-bit unsigned integer.")
 	}
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("=> Hola Estación Espacial Internacional")
-	fmt.Println("")
-	fmt.Println("")
-
 }
