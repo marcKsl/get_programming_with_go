627406a42784eb0dc5bb6bd6f8c6a9bca48947c5
marcKsl <m.kassel148@gmail.com>
2024-02-23 18:01:45 +0100

    Lesson 12.1 Functions - Function delcarations

--- main.go
+++ main.go
@@ -2,41 +2,60 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
+// A function in go consists of the following parts:
+// func FunctioName (n int) int
+// The function keyword "func" the function Name,
+// the parameters with their respective types and the return type
+
+func MyFunction(n int) int {
+	return n + 1
+}
+
 func main() {
-	cipherText := "CSOITEUIWUIZNSROCNKFD"
-	keyword := "GOLANG"
 
-	// The strings.Repeat function may come in handy.
-	// Give it a try, but also complete this exercise without
-	// importing any packages other than fmt to print the deciphered message.
-	// Try this exercise using range in a loop and again without it.
-	// Remember that the range keyword splits a string into runes,
-	// whereas an index like keyword[0] results in a byte.
-	// Tip You can only perform operations on values of the same type,
-	// but you can convert one type to the other (string, byte, rune).
-	// To wrap around at the edges of the alphabet,
-	// the Caesar cipher exercise made use of a comparison.
-	// Solve this exercise without any if statements by using modulus (%).
+	// A function call
+	// The integer 10 is passed in as an argument to the function call
+	fmt.Println(MyFunction(10))
 
-	// for loop through len(cipherText)
+	// This calls the Intn() Function from the rand-Package
+	// We can find the definition of functions in the standard package library on: https://pkg.go.dev/std
+	fmt.Println(rand.Intn(10))
 
-	extendedKeyword := ""
-	repeatCount := len(cipherText) / len(keyword)
+	// Quick reminder: Functions accept parameters and functions get called with arguments
 
-	for i := 0; i < repeatCount+1; i++ {
-		extendedKeyword += keyword
+	// Call of the function Unix(),
+	// that returns the local time as type Time,
+	// corresponding to the time passed
+	// since 01/01/1970 and the given seconds / nanoseconds
+	// as arguments to the call
+	// Note that both arguments need to be provided, or the
+	// compiler throws an error
+	fmt.Println(time.Unix(10, 0))
+
+	// A function can be called with multiple parameters like the Atoi function in the strconv package
+
+	s, err := strconv.Atoi("10")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(s)
 	}
 
-	// There is a solution that GPT suggested that involves slices
-	// We didn't use slices yet,
-	// so i stick with just adding 1 to repeatCount for simplicity
+	// A look into the future
+	// The Println() function from the fmt package is a special one
+	// It accepts multiple arguments but only one is mandatory
+	// And the types are not relevant in the function call
+	// The declaration looks like this:
+	// func Println(a ...interface{}) (n int, err error)
+	// It makes use of two things we don't know yet.
+	// One being the "...", which indicates, that this function is
+	// variadic, so it can accept a variable number of arguments in its call
+	// The next one is the empty interface "interface{}" that lets us every type.
+	// Both things will be discussed in further Lessons.
 
-	var output string
-	for i := 0; i < len(cipherText); i++ {
-		shift := (cipherText[i] - extendedKeyword[i] + 26) % 26
-		output += string('A' + shift)
-	}
-	fmt.Println(output)
 }
