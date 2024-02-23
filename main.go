b8b55ad03c632ee2b3782773aa75c1a724f92e69
marcKsl <m.kassel148@gmail.com>
2024-02-23 18:25:44 +0100

    Lesson 12.2 Writing a function

--- main.go
+++ main.go
@@ -2,60 +2,37 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"strconv"
-	"time"
 )
 
-// A function in go consists of the following parts:
-// func FunctioName (n int) int
-// The function keyword "func" the function Name,
-// the parameters with their respective types and the return type
-
-func MyFunction(n int) int {
-	return n + 1
+// kelvinToCelcius converts Kelvin to Celcius
+func kelvinToCelcius(k float64) float64 {
+	return k - 273.15
 }
 
 func main() {
+	fmt.Println("Hello World")
 
-	// A function call
-	// The integer 10 is passed in as an argument to the function call
-	fmt.Println(MyFunction(10))
+	// Note: functions from the same package are invoked without prefixing the package name
+	// Note: We do not "give" the original variable to the function,
+	// instead we give the value of the variable k in the main scope to the variable k in the function scope
+	// we could also name the variables kScope and kFunction, it would always work. This is called pass by value.
+	k := 350.44
+	c := kelvinToCelcius(k)
+	fmt.Println(c)
 
-	// This calls the Intn() Function from the rand-Package
-	// We can find the definition of functions in the standard package library on: https://pkg.go.dev/std
-	fmt.Println(rand.Intn(10))
+	// A good reason to use functions is
+	// that they allow you to encapsulate a
+	// piece of logic in a safe manner and
+	// reuse it throughout your code with a meaningful name.
 
-	// Quick reminder: Functions accept parameters and functions get called with arguments
-
-	// Call of the function Unix(),
-	// that returns the local time as type Time,
-	// corresponding to the time passed
-	// since 01/01/1970 and the given seconds / nanoseconds
-	// as arguments to the call
-	// Note that both arguments need to be provided, or the
-	// compiler throws an error
-	fmt.Println(time.Unix(10, 0))
-
-	// A function can be called with multiple parameters like the Atoi function in the strconv package
-
-	s, err := strconv.Atoi("10")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(s)
-	}
-
-	// A look into the future
-	// The Println() function from the fmt package is a special one
-	// It accepts multiple arguments but only one is mandatory
-	// And the types are not relevant in the function call
-	// The declaration looks like this:
-	// func Println(a ...interface{}) (n int, err error)
-	// It makes use of two things we don't know yet.
-	// One being the "...", which indicates, that this function is
-	// variadic, so it can accept a variable number of arguments in its call
-	// The next one is the empty interface "interface{}" that lets us every type.
-	// Both things will be discussed in further Lessons.
+	// Summary
+	//-----------------------------------
+	// Functions are declared with a name, parameters and a list of returns
+	// Capitalized function names, types and variables are made available to other packages
+	// Each parameter and result is followed by a type, types can be chained like: func MyFunction(n, m int) (e, n int)
+	// If you want to call a function in another package than the package it was declared in,
+	// you need to prefix it with the corresponding package name
+	// Functions are called with arguments that correspong to the parameters they accept.
+	// Results are returned to the caller with the return keyword
 
 }
