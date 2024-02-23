9493853d28e3f1c4f912ef7cffacf7dced3c625d
marcKsl <m.kassel148@gmail.com>
2024-02-23 18:31:26 +0100

    Lesson 12 Functions - Experiment functions.go

--- main.go
+++ main.go
@@ -4,35 +4,25 @@ import (
 	"fmt"
 )
 
-// kelvinToCelcius converts Kelvin to Celcius
 func kelvinToCelcius(k float64) float64 {
 	return k - 273.15
 }
 
+func celciusToFahrenheit(c float64) float64 {
+	return (c * 9.0 / 5.0) + 32.0
+}
+
+func kelvinToFahrenheit(k float64) float64 {
+	return celciusToFahrenheit(kelvinToCelcius(k))
+}
+
 func main() {
-	fmt.Println("Hello World")
+	k := 300.00
 
-	// Note: functions from the same package are invoked without prefixing the package name
-	// Note: We do not "give" the original variable to the function,
-	// instead we give the value of the variable k in the main scope to the variable k in the function scope
-	// we could also name the variables kScope and kFunction, it would always work. This is called pass by value.
-	k := 350.44
-	c := kelvinToCelcius(k)
-	fmt.Println(c)
+	fmt.Println(kelvinToCelcius(k))
+	fmt.Println(kelvinToFahrenheit(k))
 
-	// A good reason to use functions is
-	// that they allow you to encapsulate a
-	// piece of logic in a safe manner and
-	// reuse it throughout your code with a meaningful name.
+	c := 20.0
 
-	// Summary
-	//-----------------------------------
-	// Functions are declared with a name, parameters and a list of returns
-	// Capitalized function names, types and variables are made available to other packages
-	// Each parameter and result is followed by a type, types can be chained like: func MyFunction(n, m int) (e, n int)
-	// If you want to call a function in another package than the package it was declared in,
-	// you need to prefix it with the corresponding package name
-	// Functions are called with arguments that correspong to the parameters they accept.
-	// Results are returned to the caller with the return keyword
-
+	fmt.Println(celciusToFahrenheit(c))
 }
