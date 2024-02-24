a96f78915249a60cecb201554e160efdf493c1f2
marcKsl <m.kassel148@gmail.com>
2024-02-24 19:00:55 +0100

    Lesson 16.1 Declaring arrays and accessing their elements

--- main.go
+++ main.go
@@ -4,55 +4,34 @@ import (
 	"fmt"
 )
 
-// Two Tables, Celsius-Fahrenheit | Fahrenheit-Celsius
-// Each shows the values for -40° C to 100° C in 5° C steps and respectively for fahrenheit
-// Implement a function drawTable, that accepts a function as a parameter
-// The accepted function sets the data for each row of the table.
-
-// Save reusable code fragements in constants
-const (
-	spacer = "==========================="
-	row    = "|   %-8s |   %-8s |\n"
-)
-
-type celsius float64
-type fahrenheit float64
-
-func (c celsius) fahrenheit() fahrenheit {
-	return fahrenheit(c*5/9 + 32)
-}
-
-func (f fahrenheit) celsius() celsius {
-	return celsius((f - 32) * 9 / 5)
-}
-
-func displayCelsiusData(i int) (string, string) {
-	d := celsius(i)
-	c := fmt.Sprintf("%.1f", d)
-	f := fmt.Sprintf("%.1f", d.fahrenheit())
-	return c, f
-}
-
-func displayFahrenheitData(i int) (string, string) {
-	d := fahrenheit(i)
-	f := fmt.Sprintf("%.1f", d)
-	c := fmt.Sprintf("%.1f", d.celsius())
-	return f, c
-}
-
-func drawTable(d func(i int) (string, string)) {
-	fmt.Println(spacer)
-	fmt.Printf(row, "°C", "°F")
-	fmt.Println(spacer)
-	for i := -40; i < 101; i += 5 {
-		a, b := d(i)
-		fmt.Printf(row, a, b)
-	}
-	fmt.Println(spacer)
-}
-
 func main() {
-	drawTable(displayCelsiusData)
-	fmt.Printf("\n\n\n")
-	drawTable(displayFahrenheitData)
+	// This declares a string array named planets with a length of 12
+	var planets [12]string
+	// The following lines assign different values to different indices in the given array.
+	planets[0] = "Mercury"
+	planets[1] = "Venus"
+	planets[2] = "Earth"
+	planets[3] = "Mars"
+	planets[4] = "Jupiter"
+	planets[5] = "Saturn"
+	planets[6] = "Uranus"
+	planets[7] = "Neptune"
+
+	// We can retrieve the value at a given index in the array like so:
+	earth := planets[2]
+	// Or we can use it directly in function calls as arguments:
+	fmt.Printf("The people from %v would love to live on %v one day.\n", earth, planets[3])
+
+	// We can retrieve the length of an array like so:
+	fmt.Println(len(planets))
+	// The len() function is a built-in function in go and does not require any package imports.
+
+	// Elements of an Array are initialized with the zero value of the array's type, for example 0 for integer arrays.
+	// A complete list of all zero values:
+	// int, int8, int16, int32, int64, uint, uint8 (alias byte), uint16, uint32, uint64, uintptr: 0
+	// float32, float64: 0.0 <- Note that it includes one floating point
+	// complex64, complex128: 0+0i
+	// bool: false
+	// string: "" (leerer String) <- That's kind of interesting but i like it a lot
+	// pointers, functions, interfaces, slices, channels, maps: nil <- i really like the word nil, to be honest.
 }
