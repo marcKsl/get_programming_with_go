a267ea0e9eda1ace34e1ea1fa29912a6c96a27f1
marcKsl <m.kassel148@gmail.com>
2024-02-24 17:56:54 +0100

    Lesson 14 First-class functions - Experiment calibrate.go

--- main.go
+++ main.go
@@ -2,38 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 type kelvin float64
 
-// sensor function type
 type sensor func() kelvin
 
 func realSensor() kelvin {
 	return 0
 }
 
+func fakeSensor() kelvin {
+	return kelvin(rand.Intn(151) + 150)
+}
+
 func calibrate(s sensor, offset kelvin) sensor {
-	// We return a anonymous function, or respectively a function literal
 	return func() kelvin {
-		// We can use s and offset of a surrounding scope because
-		// anonymous functions enclose the variables in scope
-		// These variables are accessible to the anonymous function
-		// even after the calibrate function has ended.
 		return s() + offset
 	}
 }
 
 func main() {
-	sensor := calibrate(realSensor, 5)
+	var offset kelvin = 10
+	sensor := calibrate(realSensor, offset)
+	sensor2 := fakeSensor
+	fmt.Println(sensor2())
+	fmt.Println(sensor2())
+	fmt.Println(sensor2())
+	fmt.Println(sensor2())
+	fmt.Println(calibrate(sensor2, offset)())
+	fmt.Println(sensor2())
 	fmt.Println(sensor())
-	var k kelvin = 297.0
-	// The k inside the anonymous function references the k above
-	tempKelvin := func() kelvin { return k }
-	fmt.Println(tempKelvin())
-	// Therefore it can be incremented
-	k++
-	// And the function uses the incremented value, because it's like said above, just a reference
-	fmt.Println(tempKelvin())
-	// This seems to come in handy in for loops
+	// Changing the value of offset won't affect the output of the sensor() function
+	// The initial offset value was passed in by value, so it does not reference the offset value
+	// anymore.
+	offset = 20
+	fmt.Println(sensor())
 }
