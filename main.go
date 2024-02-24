21247918781937426a78301ddf19502f24850aec
marcKsl <m.kassel148@gmail.com>
2024-02-24 02:24:51 +0100

    Lesson 14.4 Closures and anonymous functions Pt.1

--- main.go
+++ main.go
@@ -2,35 +2,43 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
-type kelvin float64
-type sensor func() kelvin
-
-func measureTemperature(samples int, s sensor) {
-	for i := 0; i < samples; i++ {
-		k := s()
-		fmt.Printf("%v° K\n", k)
-		time.Sleep(time.Second)
-	}
+// Anonymous functions can be assigned to variables.
+// These variables can then be used as any other variable.
+var f = func() {
+	fmt.Println("Hello")
 }
 
-func fakeSensor() kelvin {
-	return kelvin(rand.Intn(151) + 150)
-}
-
-// Question:
-// Rewrite the following function signature to use a function type:
-// func drawTable(row int, getRow func(row int) (string, string))
-
-// Solution:
-// type getRowFn func(row int) (string, string)
-// func drawTable(rows int, getRow getRowFn) {
-// 	***Implement Function Body here***
-// }
+// The variable you declare can be in the scope of the package or within a function.
 
 func main() {
-	measureTemperature(4, fakeSensor)
+	// They can also be assigned inside the main() function like any other function.
+	var g = func() {
+		fmt.Println("sunny")
+	}
+	// They can be called like so:
+	f()
+	g()
+	// Of course they can accept parameters
+	var h = func(s string) {
+		fmt.Println(s)
+	}
+	// And then get called with arguments
+	h("World!")
+	// And of course they can specify the return type/s
+	var j = func(s string) string {
+		return "I " + s + " it!"
+	}
+	fmt.Println(j("like"))
+	var k = func(s string) (string, string) {
+		return s, s + "!"
+	}
+	m1, m2 := k("Hey")
+	fmt.Println(m1, m2)
+
+	// We can even declare and call them in one statement.
+	func() {
+		fmt.Println("One Statement, crazy.")
+	}()
 }
