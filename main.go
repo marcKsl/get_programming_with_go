feafe866e19411b996311c2b476ccb7a8b5deadc
marcKsl <m.kassel148@gmail.com>
2024-02-24 22:52:05 +0100

    Lesson 16.5 Arrays are copied

--- main.go
+++ main.go
@@ -4,26 +4,58 @@ import (
 	"fmt"
 )
 
+// Read on, come back here later when told so.
+// This function is completely useless.
+// It will take in an array as a parameter.
+// By nature in go, functions use pass by value, which means
+// the original array will not be referenced,
+// instead we modify a newly created array, that holds the same values
+// as the array that was given in as an argument.
+// But this newly created array is no longer in memory when the function ends,
+// and the original array has never been modified.
+func terraform(planets [8]string) {
+	for i := range planets {
+		planets[i] = "New" + planets[i]
+	}
+}
+
 func main() {
 	fmt.Println("Hello World")
 
-	a := [...]string{"Is this the real life?", "Is this just fantasy?"}
+	planets := [8]string{
+		"Mercury",
+		"Venus",
+		"Earth",
+		"Mars",
+		"Jupiter",
+		"Saturn",
+		"Uranus",
+		"Neptune",
+	}
 
-	// To iterate over an array there are two possible ways in go.
-	// The first one being a normal for loop that uses the len() function
-	// to determine the boundaries of the array.
-	// Note: It bears the danger to exceed the boundaries of the array if not implemented correctly.
-	// Note: The traditional for loop allows for loop customization like accessing only odd elements.
-	for i := 0; i < len(a); i++ {
-		fmt.Println(i, a[i])
-	}
-	// The second one involves the range keyword and gives us access to both,
-	// the index and the value at the specific index.
-	for i, s := range a {
-		fmt.Println(i, s)
-	}
-	// If we do not need the index variable, we can use the blank identifier.
-	for _, s := range a {
-		fmt.Println(s)
-	}
+	moonAndEarth := [2]string{"Moon", "Earth"}
+
+	// Assigning an array to a new variable will make a copy of it.
+	planets2 := planets
+
+	planets[2] = "Azeroth"
+
+	fmt.Println(planets)
+	// No Azeroth here, still the good ol' earthy
+	// Because we do not reference the original array,
+	// instead we reference a newly created array,
+	// that contains the same values as planets.
+	fmt.Println(planets2)
+
+	// This does not change / do anything, see explanation above.
+	terraform(planets)
+	fmt.Printf("No new planets at all: %v\n", planets)
+
+	// It is also important to notice, that the length of an array is part of its type.
+	// So if we have two arrays, both are collections of strings,
+	// but don't share the same length, we cannot assign one to each other.
+	fmt.Println(moonAndEarth)
+	fmt.Printf("The moonAndEarth array is: %v long.\n", len(moonAndEarth))
+	fmt.Printf("The planets array is: %v long.\n", len(planets))
+	fmt.Println("")
 }
