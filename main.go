512d84c5db7f96cc09629c7689cf83d44520ba28
marcKsl <m.kassel148@gmail.com>
2024-02-24 02:56:21 +0100

    Lesson 14.4 Closures and anonymous functions Pt.2

--- main.go
+++ main.go
@@ -4,41 +4,36 @@ import (
 	"fmt"
 )
 
-// Anonymous functions can be assigned to variables.
-// These variables can then be used as any other variable.
-var f = func() {
-	fmt.Println("Hello")
+type kelvin float64
+
+// sensor function type
+type sensor func() kelvin
+
+func realSensor() kelvin {
+	return 0
 }
 
-// The variable you declare can be in the scope of the package or within a function.
+func calibrate(s sensor, offset kelvin) sensor {
+	// We return a anonymous function, or respectively a function literal
+	return func() kelvin {
+		// We can use s and offset of a surrounding scope because
+		// anonymous functions enclose the variables in scope
+		// These variables are accessible to the anonymous function
+		// even after the calibrate function has ended.
+		return s() + offset
+	}
+}
 
 func main() {
-	// They can also be assigned inside the main() function like any other function.
-	var g = func() {
-		fmt.Println("sunny")
-	}
-	// They can be called like so:
-	f()
-	g()
-	// Of course they can accept parameters
-	var h = func(s string) {
-		fmt.Println(s)
-	}
-	// And then get called with arguments
-	h("World!")
-	// And of course they can specify the return type/s
-	var j = func(s string) string {
-		return "I " + s + " it!"
-	}
-	fmt.Println(j("like"))
-	var k = func(s string) (string, string) {
-		return s, s + "!"
-	}
-	m1, m2 := k("Hey")
-	fmt.Println(m1, m2)
-
-	// We can even declare and call them in one statement.
-	func() {
-		fmt.Println("One Statement, crazy.")
-	}()
+	sensor := calibrate(realSensor, 5)
+	fmt.Println(sensor())
+	var k kelvin = 297.0
+	// The k inside the anonymous function references the k above
+	tempKelvin := func() kelvin { return k }
+	fmt.Println(tempKelvin())
+	// Therefore it can be incremented
+	k++
+	// And the function uses the incremented value, because it's like said above, just a reference
+	fmt.Println(tempKelvin())
+	// This seems to come in handy in for loops
 }
