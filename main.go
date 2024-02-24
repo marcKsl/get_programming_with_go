6dc9edccaa4a8c6fd0692d70451628d5f716ddf2
marcKsl <m.kassel148@gmail.com>
2024-02-24 23:18:13 +0100

    Lesson 16 Arrayed in splendor - Experiment chess.go

--- main.go
+++ main.go
@@ -5,22 +5,31 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
-
-	// We can also store arrays in arrays.
-
-	var board [8][8]string
-
-	board[0][0] = "r"
-	board[0][7] = "r"
-
-	for column := range board[1] {
-		board[1][column] = "p"
+	var board [8][8]rune
+	specialRow := [8]rune{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'}
+	var pawnRow [8]rune
+	for i := range pawnRow {
+		pawnRow[i] = 'p'
 	}
 
-	fmt.Print(board)
+	for i := range board {
+		for j := range board[i] {
+			board[i][j] = 'o'
+		}
+	}
 
-	// Or for example in a sudoku game we need a 9x9 grid.
-	var sudokuBoard [9][9]int
-	fmt.Println(sudokuBoard)
+	for i := range board[0] {
+		board[0][i] = specialRow[i]
+		board[1][i] = pawnRow[i]
+		// Substract 32 from the lowercase rune to get to the uppercase rune in ASCII
+		board[7][i] = specialRow[i] - 32
+		board[6][i] = pawnRow[i] - 32
+	}
+
+	for i := range board {
+		for j := range board[i] {
+			fmt.Printf("%-3c", board[i][j])
+		}
+		fmt.Printf("\n")
+	}
 }
