59c1f506312d36d4673076d77632555e9fe23017
marcKsl <m.kassel148@gmail.com>
2024-02-24 20:28:33 +0100

    Lesson 16.4 Iterating through arrays

--- main.go
+++ main.go
@@ -7,25 +7,23 @@ import (
 func main() {
 	fmt.Println("Hello World")
 
-	// Composite literals in go are a special syntax known by the compiler to
-	// declare a collection type and directly initialize it with values.
-	a := [4]int{0, 1, 2, 3}
-	// This line does not compile in go, cause the initialization goes out of bounds
-	// c := [3]int{0, 1, 2, 3, 4}
-	// This is absolutely fine, the remaining indices get initialized
-	// with the zero value of the type, in the case of int it is 0
-	b := [7]int{0, 1, 2, 3, 4}
-	// We can span the initializationb across multiple lines in go.
-	// We can also use the ellipsis to tell go that it
-	// should pick the correct size for the array instead of providing an integer
-	c := [...]string{
-		"mamamia",
-		"here i go again",
-		"my, my,",
-		"how can i resist you?",
+	a := [...]string{"Is this the real life?", "Is this just fantasy?"}
+
+	// To iterate over an array there are two possible ways in go.
+	// The first one being a normal for loop that uses the len() function
+	// to determine the boundaries of the array.
+	// Note: It bears the danger to exceed the boundaries of the array if not implemented correctly.
+	// Note: The traditional for loop allows for loop customization like accessing only odd elements.
+	for i := 0; i < len(a); i++ {
+		fmt.Println(i, a[i])
 	}
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(len(c))
+	// The second one involves the range keyword and gives us access to both,
+	// the index and the value at the specific index.
+	for i, s := range a {
+		fmt.Println(i, s)
+	}
+	// If we do not need the index variable, we can use the blank identifier.
+	for _, s := range a {
+		fmt.Println(s)
+	}
 }
