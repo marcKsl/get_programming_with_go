c93cd558ac86def5403630325a03e920e441f95a
marcKsl <m.kassel148@gmail.com>
2024-02-24 18:43:16 +0100

    Lesson 15 Capstone: Temperature tables

--- main.go
+++ main.go
@@ -2,41 +2,57 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
-type kelvin float64
+// Two Tables, Celsius-Fahrenheit | Fahrenheit-Celsius
+// Each shows the values for -40° C to 100° C in 5° C steps and respectively for fahrenheit
+// Implement a function drawTable, that accepts a function as a parameter
+// The accepted function sets the data for each row of the table.
 
-type sensor func() kelvin
+// Save reusable code fragements in constants
+const (
+	spacer = "==========================="
+	row    = "|   %-8s |   %-8s |\n"
+)
 
-func realSensor() kelvin {
-	return 0
+type celsius float64
+type fahrenheit float64
+
+func (c celsius) fahrenheit() fahrenheit {
+	return fahrenheit(c*5/9 + 32)
 }
 
-func fakeSensor() kelvin {
-	return kelvin(rand.Intn(151) + 150)
+func (f fahrenheit) celsius() celsius {
+	return celsius((f - 32) * 9 / 5)
 }
 
-func calibrate(s sensor, offset kelvin) sensor {
-	return func() kelvin {
-		return s() + offset
+func displayCelsiusData(i int) (string, string) {
+	d := celsius(i)
+	c := fmt.Sprintf("%.1f", d)
+	f := fmt.Sprintf("%.1f", d.fahrenheit())
+	return c, f
+}
+
+func displayFahrenheitData(i int) (string, string) {
+	d := fahrenheit(i)
+	f := fmt.Sprintf("%.1f", d)
+	c := fmt.Sprintf("%.1f", d.celsius())
+	return f, c
+}
+
+func drawTable(d func(i int) (string, string)) {
+	fmt.Println(spacer)
+	fmt.Printf(row, "°C", "°F")
+	fmt.Println(spacer)
+	for i := -40; i < 101; i += 5 {
+		a, b := d(i)
+		fmt.Printf(row, a, b)
 	}
+	fmt.Println(spacer)
 }
 
 func main() {
-	var offset kelvin = 10
-	sensor := calibrate(realSensor, offset)
-	sensor2 := fakeSensor
-	fmt.Println(sensor2())
-	fmt.Println(sensor2())
-	fmt.Println(sensor2())
-	fmt.Println(sensor2())
-	fmt.Println(calibrate(sensor2, offset)())
-	fmt.Println(sensor2())
-	fmt.Println(sensor())
-	// Changing the value of offset won't affect the output of the sensor() function
-	// The initial offset value was passed in by value, so it does not reference the offset value
-	// anymore.
-	offset = 20
-	fmt.Println(sensor())
+	drawTable(displayCelsiusData)
+	fmt.Printf("\n\n\n")
+	drawTable(displayFahrenheitData)
 }
