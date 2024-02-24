6b59c65a328d5df45ac24863165bb4477a1ee301
marcKsl <m.kassel148@gmail.com>
2024-02-24 20:18:32 +0100

    Lesson 16.3 Initialize arrays with composite literals

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// Arrays are of a fixed size in Go.
-	// If you try to access an array out of bounds,
-	// the Go compiler will report an error.
-	var planets [8]string
-	// These lines do not work in Go:
-	// planets[8] = "Pluto"
-	// pluto := planets[8]
-	// Be careful, the above lines do not work in Go.
-	// If for some reason the go compiler cannot detect the error,
-	// the program will panic during runtime. That is good behaviour
-	// because it ensures that no unspecified memory operation will be performed,
-	// as is the case in c
-	// Always stay safe and access arrays inside their boundaries:
-	planets[7] = "Neptune"
-	fmt.Println(planets[7])
+	fmt.Println("Hello World")
+
+	// Composite literals in go are a special syntax known by the compiler to
+	// declare a collection type and directly initialize it with values.
+	a := [4]int{0, 1, 2, 3}
+	// This line does not compile in go, cause the initialization goes out of bounds
+	// c := [3]int{0, 1, 2, 3, 4}
+	// This is absolutely fine, the remaining indices get initialized
+	// with the zero value of the type, in the case of int it is 0
+	b := [7]int{0, 1, 2, 3, 4}
+	// We can span the initializationb across multiple lines in go.
+	// We can also use the ellipsis to tell go that it
+	// should pick the correct size for the array instead of providing an integer
+	c := [...]string{
+		"mamamia",
+		"here i go again",
+		"my, my,",
+		"how can i resist you?",
+	}
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(len(c))
 }
