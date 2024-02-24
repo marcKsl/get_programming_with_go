dee0d2dad8a5f88d0ddf101926a5cc1ae3859425
marcKsl <m.kassel148@gmail.com>
2024-02-24 19:51:20 +0100

    Lesson 16.2 Don't go out of bounds

--- main.go
+++ main.go
@@ -1,37 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	// This declares a string array named planets with a length of 12
-	var planets [12]string
-	// The following lines assign different values to different indices in the given array.
-	planets[0] = "Mercury"
-	planets[1] = "Venus"
-	planets[2] = "Earth"
-	planets[3] = "Mars"
-	planets[4] = "Jupiter"
-	planets[5] = "Saturn"
-	planets[6] = "Uranus"
+	// Arrays are of a fixed size in Go.
+	// If you try to access an array out of bounds,
+	// the Go compiler will report an error.
+	var planets [8]string
+	// These lines do not work in Go:
+	// planets[8] = "Pluto"
+	// pluto := planets[8]
+	// Be careful, the above lines do not work in Go.
+	// If for some reason the go compiler cannot detect the error,
+	// the program will panic during runtime. That is good behaviour
+	// because it ensures that no unspecified memory operation will be performed,
+	// as is the case in c
+	// Always stay safe and access arrays inside their boundaries:
 	planets[7] = "Neptune"
-
-	// We can retrieve the value at a given index in the array like so:
-	earth := planets[2]
-	// Or we can use it directly in function calls as arguments:
-	fmt.Printf("The people from %v would love to live on %v one day.\n", earth, planets[3])
-
-	// We can retrieve the length of an array like so:
-	fmt.Println(len(planets))
-	// The len() function is a built-in function in go and does not require any package imports.
-
-	// Elements of an Array are initialized with the zero value of the array's type, for example 0 for integer arrays.
-	// A complete list of all zero values:
-	// int, int8, int16, int32, int64, uint, uint8 (alias byte), uint16, uint32, uint64, uintptr: 0
-	// float32, float64: 0.0 <- Note that it includes one floating point
-	// complex64, complex128: 0+0i
-	// bool: false
-	// string: "" (leerer String) <- That's kind of interesting but i like it a lot
-	// pointers, functions, interfaces, slices, channels, maps: nil <- i really like the word nil, to be honest.
+	fmt.Println(planets[7])
 }
