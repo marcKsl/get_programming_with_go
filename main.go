7eac6c620f0aae117f8be4a3d6dc6ecf79f91d20
marcKsl <m.kassel148@gmail.com>
2024-02-25 01:58:34 +0100

    Lesson 17.1 Slicing an array

--- main.go
+++ main.go
@@ -1,35 +1,80 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var board [8][8]rune
-	specialRow := [8]rune{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'}
-	var pawnRow [8]rune
-	for i := range pawnRow {
-		pawnRow[i] = 'p'
+	planets := [...]string{
+		"Mercury",
+		"Venus",
+		"Earth",
+		"Mars",
+		"Jupiter",
+		"Saturn",
+		"Uranus",
+		"Neptune",
 	}
+	// This will create a slice of integers.
+	// Note: It will go up to the 4th index, but it will not include it.
+	terrestrial := planets[0:4]
+	gasGiants := planets[4:6]
+	iceGiants := planets[6:8]
 
-	for i := range board {
-		for j := range board[i] {
-			board[i][j] = 'o'
-		}
-	}
+	fmt.Println(planets)
+	fmt.Println(terrestrial)
+	fmt.Println(gasGiants)
+	fmt.Println(iceGiants)
 
-	for i := range board[0] {
-		board[0][i] = specialRow[i]
-		board[1][i] = pawnRow[i]
-		// Substract 32 from the lowercase rune to get to the uppercase rune in ASCII
-		board[7][i] = specialRow[i] - 32
-		board[6][i] = pawnRow[i] - 32
-	}
+	// We can normally index into slices, like with arrays:
+	fmt.Println(terrestrial[2])
 
-	for i := range board {
-		for j := range board[i] {
-			fmt.Printf("%-3c", board[i][j])
-		}
-		fmt.Printf("\n")
-	}
+	// Slices can be sliced into more slices:
+	giants := planets[4:8]
+	gas := giants[0:2]
+	ice := giants[2:4]
+
+	fmt.Println(giants)
+	fmt.Println(gas)
+	fmt.Println(ice)
+
+	// A slice always references an array,
+	// so if we modify the slice, we modify the underlying array,
+	// so that the change will be visible in all other slices too.
+	giants[0] = "Oops, i di..."
+	fmt.Println(planets)
+	fmt.Println(giants)
+	fmt.Println(gas)
+	fmt.Println(ice)
+
+	// If we omit the first index when slicing an array, it defaults to the beginning of the array.
+	// If we omit the last index of the array, it defaults to the length of the array.
+
+	terrestrial2 := planets[:4]
+	gasGiants2 := planets[4:6]
+	iceGiants2 := planets[6:]
+	fmt.Println(terrestrial2)
+	fmt.Println(gasGiants2)
+	fmt.Println(iceGiants2)
+
+	// Ommiting both indices leads to a slice containing the whole array.
+	allPlanets := planets[:]
+	fmt.Println(allPlanets)
+
+	// Slice indices may not be negative.
+
+	// Strings can also be sliced.
+
+	neptune := "Neptune"
+	// This unlike with arrays creates a new variable and no reference.
+	tune := neptune[3:]
+	fmt.Println(tune)
+	// So this new assertion will not affect the "tune" variable.
+	neptune = "Moon"
+	fmt.Println(tune)
+
+	//  Note: Indices indicate the number of bytes, not runes.
+	question := "¿Cómo estás?"
+	fmt.Println(question[:6])
+
+	colonized := terrestrial[2:]
+	fmt.Println(colonized)
 }
