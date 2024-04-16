bfb29f3f24e60471f2e34629909a1c99c6640ce1
marcKsl <marc.kassel@outlook.com>
2024-04-16 23:24:07 +0200

    Lesson 21 complete with example

--- main.go
+++ main.go
@@ -1,143 +1,178 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"math/rand"
-	"time"
+	"os"
 )
 
-const (
-	width  = 80
-	height = 15
-)
-
-type Universe [][]bool
-
-func NewUniverse(height int, width int) Universe {
-	u := make([][]bool, height)
-	for i := range u {
-		u[i] = make([]bool, width)
-	}
-	return u
+type location struct {
+	lat  float64
+	long float64
 }
 
-func (u Universe) Show() {
-	for i := range u {
-		fmt.Printf("|")
-		for j := range u[i] {
-			if u[i][j] {
-				fmt.Printf("*")
-			} else {
-				fmt.Printf(" ")
-			}
-		}
-		fmt.Printf("|")
-		fmt.Printf("\n")
+func exitOnError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-}
-
-func (u Universe) Seed() {
-	for i := range u {
-		for j := range u[i] {
-			n := rand.Intn(4) // is either 0, 1, 2, 3
-			switch n {
-			case 0:
-				u[i][j] = false
-			case 1:
-				u[i][j] = false
-			case 2:
-				u[i][j] = false
-			default:
-				u[i][j] = true
-			}
-		}
-	}
-}
-
-func (u Universe) Alive(x, y int) bool {
-	x = (x + width) % width
-	y = (y + height) % height
-	return u[y][x]
-}
-
-func (u Universe) Neighbors(x, y int) int {
-	i := 0
-	for dx := -1; dx <= 1; dx++ {
-		for dy := -1; dy <= 1; dy++ {
-			if dx == 0 && dy == 0 {
-				continue
-			}
-			if u.Alive(dx+x, dy+y) {
-				i++
-			}
-
-		}
-	}
-	return i
-}
-
-func (u Universe) Next(x, y int) bool {
-	count := u.Neighbors(x, y)
-	if u.Alive(x, y) {
-		switch {
-		case count < 2:
-			return false
-		case count > 3:
-			return false
-		default:
-			return true
-		}
-	} else {
-		if count == 3 {
-			return true
-		}
-		return false
-	}
-}
-
-func Step(a, b Universe) {
-	for i := range a {
-		for j := range a[i] {
-			b[i][j] = a.Next(j, i)
-		}
-	}
-}
-
-func (u Universe) totalElements() int {
-	return len(u) * len(u[0])
-}
-
-func (u Universe) allDeadOrAlive() bool {
-	count := 0
-	for i := range u {
-		for j := range u[i] {
-			if !u.Alive(j, i) {
-				count++
-			}
-		}
-	}
-	if count == 0 || count == u.totalElements() {
-		return true
-	}
-	return false
 }
 
 func main() {
-	// Conways game of life.
-	fmt.Println("\033[H") // clears terminal on mac
-	u := NewUniverse(height, width)
-	u.Seed()
-	u.Show()
-	u2 := u
-	for {
-		time.Sleep(time.Millisecond * 500)
-		Step(u, u2)
-		fmt.Println("\033[H") // clears terminal on mac
-		u2.Show()
-		u = u2
-		if u.allDeadOrAlive() {
-			break
-		}
+
+	fmt.Println("-------------------------------------------------------")
+	fmt.Println("------------Declaring a structure----------------------")
+	fmt.Println("-------------------------------------------------------")
+
+	var curiosity struct {
+		lat  float64
+		long float64
 	}
 
+	curiosity.lat = -4.5895
+	curiosity.long = 137.4417
+
+	fmt.Println(curiosity.lat, curiosity.long)
+	fmt.Println(curiosity)
+
+	fmt.Println("-------------------------------------------------------")
+	fmt.Println("------------Reusing structures with types--------------")
+	fmt.Println("-------------------------------------------------------")
+
+	type location struct {
+		lat  float64
+		long float64
+	}
+
+	var spirit location
+	spirit.lat = -14.5684
+	spirit.long = 175.472636
+
+	var opportunity location
+	opportunity.lat = -1.9462
+	opportunity.long = 354.4734
+
+	fmt.Println(spirit, opportunity)
+	fmt.Printf("Type of spririt: %T\n", spirit)
+	fmt.Printf("Type of opportunity: %T\n", opportunity)
+
+	fmt.Println("-------------------------------------------------------")
+	fmt.Println("-----Initialize structures with composite literals-----")
+	fmt.Println("-------------------------------------------------------")
+
+	type location2 struct {
+		lat, long float64
+	}
+
+	opportunity2 := location{lat: -1.9462, long: 354.4734}
+	fmt.Println(opportunity2)
+
+	insight := location{lat: 4.5, long: 135.9}
+	fmt.Println(insight)
+
+	// It is also possible to omit the keys and only pass the values,
+	// use with caution tho, it can get pretty unreadable
+
+	newthing := location{-44.5544, 754.7574}
+	fmt.Println(newthing)
+
+	// %v format verb with + prints also the field names
+
+	fmt.Printf("%v\n", newthing)
+	fmt.Printf("%+v\n", newthing)
+
+	fmt.Println("-------------------------------------------------------")
+	fmt.Println("------------Structures are copied----------------------")
+	fmt.Println("-------------------------------------------------------")
+
+	bradbury := location{-4.5895, 137.4417}
+	curiosity2 := bradbury
+
+	myRover := bradbury
+
+	fmt.Printf("My Rover: %+v\n", myRover)
+
+	curiosity2.long += 0.0106
+
+	fmt.Println(bradbury, curiosity2)
+
+	// Structs passed into functions also get copied
+
+	fmt.Println("-------------------------------------------------------")
+	fmt.Println("------------A slice of structures----------------------")
+	fmt.Println("-------------------------------------------------------")
+
+	type location3 struct {
+		name string
+		lat  float64
+		long float64
+	}
+
+	locations3 := []location3{
+		{name: "Bradbury Landing", lat: -4.5895, long: 137.4417},
+		{name: "Columbia Memorial Station", lat: -14.5684, long: 175.472636},
+		{name: "Challenger Memorial Station", lat: -1.9462, long: 354.4734},
+	}
+
+	for i := range locations3 {
+		fmt.Printf("%+v\n", locations3[i])
+	}
+
+	fmt.Println("-------------------------------------------------------")
+	fmt.Println("------------Encoding structures to JSON----------------")
+	fmt.Println("-------------------------------------------------------")
+
+	// Keys need to be uppercase to be exported in Go,
+	// so that the JSON package can access them.
+	type location4 struct {
+		Lat  float64 `json:"labernicht"`
+		Long float64 `json:"dochisso"`
+	}
+
+	curiosity4 := location4{-4.5895, 137.4417}
+
+	bytes, err := json.Marshal(curiosity4)
+	exitOnError(err)
+
+	fmt.Println(string(bytes))
+
+	fmt.Println("-------------------------------------------------------")
+	fmt.Println("-----------Customizing JSON with struct tags-----------")
+	fmt.Println("-------------------------------------------------------")
+
+	type location5 struct {
+		// struct tags are strings appended to an entry containing key value pairs,
+		// that often correspond to a specific package
+
+		Lat  float64 `json:"latitude"`
+		Long float64 `json:"longitude"`
+	}
+
+	curiosity5 := location5{-4.5895, 137.4417}
+
+	bytes2, err2 := json.Marshal(curiosity5)
+	exitOnError(err2)
+
+	fmt.Println(string(bytes2))
+
+	fmt.Println("-------------------------------------------------------")
+	fmt.Println("------------experiment landing.go----------------------")
+	fmt.Println("-------------------------------------------------------")
+
+	type location10 struct {
+		Name string  `json:"name"`
+		Lat  float64 `json:"latitude"`
+		Long float64 `json:"longitude"`
+	}
+
+	locations10 := []location10{
+		{Name: "Bradbury Landing", Lat: -4.5895, Long: 137.4417},
+		{Name: "Columbia Memorial Station", Lat: -14.5684, Long: 175.472636},
+		{Name: "Challenger Memorial Station", Lat: -1.9462, Long: 354.4734},
+	}
+
+	bytes10, err10 := json.MarshalIndent(locations10, "", "")
+	exitOnError(err10)
+
+	fmt.Println(string(bytes10))
 }
