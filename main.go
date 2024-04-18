b4b54b2f5293de7cd94292c0590bb5a53123f480
marcKsl <marc.kassel@outlook.com>
2024-04-18 20:16:53 +0200

    Lesson 24.1-2

--- main.go
+++ main.go
@@ -2,60 +2,55 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
+	"time"
 )
 
 type rover struct {
-	name string
-	gps
 }
 
-type gps struct {
-	currentLocation     location
-	destinationLocation location
-	world
+func (r rover) talk() string {
+	return "vroom vroom"
 }
 
-func (g gps) distance() float64 {
-	return g.world.distance(g.currentLocation, g.destinationLocation)
+type talker interface {
+	talk() string
 }
 
-func (g gps) message() {
-	fmt.Printf("%v kilometers remaining to the destination: %v", g.distance(), g.destinationLocation.name)
+func shout(t talker) {
+	louder := strings.ToUpper(t.talk())
+	fmt.Println(louder)
 }
 
-type location struct {
-	name      string
-	lat, long float64
+type stardater interface {
+	YearDay() int
+	Hour() int
 }
 
-func (l location) description() string {
-	return fmt.Sprintf("Location: %v: %v, %v", l.name, l.lat, l.long)
+func stardate(t stardater) float64 {
+	doy := float64(t.YearDay())
+	h := float64(t.Hour()) / 24.0
+	return 1000 + doy + h
 }
 
-type world struct {
-	name   string
-	radius float64
+type sol int
+
+func (s sol) YearDay() int {
+	return int(s % 668)
 }
 
-// distance calculation using the Spherical Law of Cosines.
-func (w world) distance(p1, p2 location) float64 {
-	s1, c1 := math.Sincos(rad(p1.lat))
-	s2, c2 := math.Sincos(rad(p2.lat))
-	clong := math.Cos(rad(p1.long - p2.long))
-	return w.radius * math.Acos(s1*s2+c1*c2*clong)
-}
-
-// rad converts degrees to radians.
-func rad(deg float64) float64 {
-	return deg * math.Pi / 180
+func (s sol) Hour() int {
+	return 0
 }
 
 func main() {
-	bradburryLanding := location{lat: -4.5895, long: 137.4417}
-	elysiumPlanitia := location{lat: 4.5, long: 135.9}
-	mars := world{name: "Mars", radius: 3389.5}
-	gps := gps{currentLocation: bradburryLanding, destinationLocation: elysiumPlanitia, world: mars}
-	curiosity := rover{name: "Curiosity", gps: gps}
-	curiosity.message()
+	r := rover{}
+	shout(r)
+
+	day := time.Date(2012, 8, 6, 5, 17, 0, 0, time.UTC)
+	fmt.Printf("%.1f Curiosity has landed\n", stardate(day))
+
+	s := sol(1422)
+	fmt.Printf("%.1f Happy birthday\n", stardate(s))
+
 }
