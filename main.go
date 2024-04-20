8c4bf233b95b67d07be1ec0db4c5c050a325c1b2
marcKsl <marc.kassel@outlook.com>
2024-04-20 10:02:03 +0200

    Lesson 24.4 experiment marshal.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type location struct {
 	lat, long coordinate
@@ -17,14 +20,51 @@ type coordinate struct {
 	h       rune
 }
 
+func (c coordinate) decimal() float64 {
+	sign := 1.0
+	switch c.h {
+	case 'S', 'W', 's', 'w':
+		sign = -1
+	}
+	return sign * (c.d + c.m/60 + c.s/3600)
+}
+
+func (c coordinate) MarshalJSON() ([]byte, error) {
+	// Calculate decimal degrees
+	decimal := c.decimal()
+
+	// We use a map, to create the existing fields.
+	jsonData := map[string]interface{}{
+		"decimal":    decimal,
+		"dms":        c.String(),
+		"degrees":    int(c.d),
+		"minutes":    int(c.m),
+		"seconds":    int(c.s),
+		"hemisphere": string(c.h),
+	}
+	return json.Marshal(jsonData)
+}
+
 func (c coordinate) String() string {
 	// %v prints the variable and %c prints the character
 	return fmt.Sprintf("%v°%v'%v''%c", c.d, c.m, c.s, c.h)
 }
 
 func main() {
-	cologneLatitude := coordinate{50, 56, 6.6228, 'N'}
-	cologneLongitude := coordinate{6, 57, 11.1636, 'E'}
-	cologne := location{cologneLatitude, cologneLongitude}
-	fmt.Println(cologne)
+	coord := coordinate{
+		d: 135,
+		m: 54,
+		s: 0,
+		h: 'E',
+	}
+
+	// Marshal the coordinate instance to JSON
+	jsonData, err := json.Marshal(coord)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the JSON data
+	fmt.Println(string(jsonData))
 }
