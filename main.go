57c8dc54e5ccbd7a3856ce06f4973a10705385f3
marcKsl <marc.kassel@outlook.com>
2024-04-21 02:28:37 +0200

    Lesson 26.3

--- main.go
+++ main.go
@@ -1,74 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type stats struct {
+	level             int
+	endurance, health int
+}
+
+func levelUp(s *stats) {
+	s.level++
+	s.endurance = 42 + (14 + s.level)
+	s.health = 5 * s.endurance
+}
+
+type character struct {
+	name  string
+	stats stats
+}
+
+type person struct {
+	name, superpower string
+	age              int
+}
+
+func (p *person) birthday() {
+	p.age++
+}
+
+func birthday(p *person) {
+	p.age++
+}
+
+func (p person) fakeBirthday() {
+	p.age++
+}
 
 func main() {
-	var administrator *string
-	scolese := "CJ Scolese"
-	administrator = &scolese
-	fmt.Println(*administrator)
-
-	bolden := "CF Bolden"
-	administrator = &bolden
-	fmt.Println(*administrator)
-
-	*administrator = "C Johnson"
-	fmt.Println(bolden)
-
-	major := administrator
-	*major = "Major Tom"
-	fmt.Println(bolden)
-
-	lightfoot := "RM Lightfoot"
-	administrator = &lightfoot
-	// major still points to bolden, because it is a copy
-	fmt.Println(administrator == major)
-
-	charles := *major
-	*major = "Charles Bolden"
-	// This prints Major Tom, correct
-	fmt.Println(charles)
-	// This prints Charles Bolden, correct
-	fmt.Println(bolden)
-
-	charles = "Charles Bolden"
-	fmt.Println(charles == bolden)
-	fmt.Println(&charles == &bolden)
-
-	type person struct {
-		name, superpower string
-		age              int
+	rebecca := person{
+		name:       "Rebecca",
+		superpower: "imagination",
+		age:        14,
 	}
 
-	timmy := &person{
-		name: "Timmothy",
-		age:  10,
+	birthday(&rebecca)
+
+	fmt.Printf("%+v\n", rebecca)
+
+	// Same for methods
+	nathan := &person{
+		name: "Nathan",
+		age:  17,
 	}
 
-	timmy2 := person{
-		name: "Timmothy2",
-		age:  10,
+	nathan.birthday()
+	fmt.Printf("%+v\n", *nathan)
+
+	// Even this works, because methods will automatically dereference the adress for the pointer receiver
+	terry := person{
+		name: "Terry",
+		age:  15,
 	}
+	// We can just call birthday
+	terry.birthday()
+	fmt.Printf("%+v\n", terry)
 
-	timmy.superpower = "flying dragons"
-	(*timmy).superpower = "riding horses"
+	// But it is important that, the receiver of the birthday() method needs to be a pointer,
+	// otherwise age really would not increment, because it would operate on a copy
+	terry.fakeBirthday()
+	fmt.Printf("%+v\n", terry)
 
-	timmy2.superpower = "flying dragons"
-	// This doesn't work, because it is not a pointer, instead it is a real instance of person
-	// (*timmy2).superpower = "riding horses"
+	// interior pointers point to inner items of structs
+	player := character{name: "Matthias"}
+	levelUp(&player.stats)
+	fmt.Printf("player is: %+v\n", player)
+	playerStatAdress := &player.stats
+	levelUp(playerStatAdress)
+	playerStatAdressValue := *playerStatAdress
+	fmt.Printf("player.stats after one more birthday is: %+v\n", player.stats)
+	fmt.Printf("playerStatAdressValue is: %+v\n", playerStatAdressValue)
+	levelUp(&player.stats)
+	fmt.Printf("playerStatAdressValue after one original player levelup is: %+v\n", playerStatAdressValue)
+	fmt.Printf("player after two original player levelup is: %+v\n", player.stats)
 
-	fmt.Printf("%v, type: %[1]T", timmy)
-	fmt.Printf("%v, type: %[1]T", timmy2)
-
-	nonsense := &[]int{10}
-	fmt.Println(nonsense)
-
-	superpowers := &[3]string{"flying", "invisibility", "super strength"}
-
-	// If it is a slice this needs to be done, because slices do not seem to
-	// dereference automatically, maps also not
-	// But both can be prefixed with & to create a pointer, only with composite literal tho
-	fmt.Println((*superpowers)[0])
-	// This only works for arrays not for slices
-	fmt.Println(superpowers[0])
 }
