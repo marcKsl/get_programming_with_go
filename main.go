7e826a1c33c796787290bbb1b8daad186b64f6d5
marcKsl <marc.kassel@outlook.com>
2024-04-22 01:15:56 +0200

    Lesson 26.6

--- main.go
+++ main.go
@@ -2,41 +2,77 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"time"
 )
 
-type talker interface {
-	talk() string
+// It's a nice example, could test user input and error handling here pretty good.
+
+type turtle struct {
+	x, y int
 }
 
-func shout(t talker) {
-	louder := strings.ToUpper(t.talk())
-	fmt.Println(louder)
+type beach [][]bool
+
+func (b *beach) showTurtle(t turtle) {
+	for i := range *b {
+		for j := range (*b)[i] {
+			if j == t.x && i == t.y {
+				fmt.Printf("*")
+			} else {
+				if (*b)[i][j] {
+					fmt.Printf(" ")
+				} else {
+					fmt.Printf(".")
+				}
+			}
+		}
+		fmt.Println()
+	}
+	time.Sleep(time.Millisecond * 200)
+	fmt.Println("\033[H")
 }
 
-type martian struct{}
-
-func (m martian) talk() string {
-	return "yip yip"
+func (t *turtle) move(direction int) {
+	if direction > 3 {
+		return
+	}
+	switch direction {
+	case 0:
+		(*t).y += 1
+	case 1:
+		(*t).x += 2
+	case 2:
+		(*t).y -= 1
+	case 3:
+		(*t).x -= 2
+	}
 }
 
-type laser struct{}
-
-func (l *laser) talk() string {
-	return "pew pew"
+func initialize(w, h int) beach {
+	b := make([][]bool, h)
+	for i := range b {
+		b[i] = make([]bool, w)
+	}
+	return b
 }
 
 func main() {
-	m := martian{}
-	shout(m)
-	shout(&m)
-
-	l := laser{}
-	// this does not work,
-	// because the receiver of the talk() method on laser is of type pointer
-	// laser...pointer, badumm
-	// shout(l)
-
-	// This works fine, because it satisfies the receiver type
-	shout(&l)
+	beach := initialize(60, 20)
+	turtle := turtle{20, 10}
+	for i := 0; i <= 10; i++ {
+		beach.showTurtle(turtle)
+		turtle.move(1)
+	}
+	for i := 0; i <= 3; i++ {
+		beach.showTurtle(turtle)
+		turtle.move(2)
+	}
+	for i := 0; i <= 10; i++ {
+		beach.showTurtle(turtle)
+		turtle.move(3)
+	}
+	for i := 0; i <= 3; i++ {
+		beach.showTurtle(turtle)
+		turtle.move(0)
+	}
 }
