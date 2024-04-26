15a9c898cd42876627f47c9c8968c8d97f84f1d3
Marc Kassel <80926538+marcKsl@users.noreply.github.com>
2024-04-26 19:58:22 +0200

    Lesson 28.3

--- main.go
+++ main.go
@@ -1,80 +1,94 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
+	"strings"
 )
 
-type safeWriter struct {
-	// We can pass any type to w that implements the io.Writer interface
-	// For example os.File does implement the io.Writer interface, because
-	// it contains a method Write(b []byte) n int, err error, which implicitly
-	// implements the io.Writer interface
-	w   io.Writer
-	err error
+// This is typical for go
+// ErrSomething will be declared and exported at package level
+var (
+	ErrBounds = errors.New("out of bounds")
+	ErrDigit  = errors.New("invalid digit")
+)
+
+// We can also create custom error types
+type SudokuError []error
+
+// It just needs an Error() string method
+func (se SudokuError) Error() string {
+	var s []string
+	// we cycle through each error in the error slice
+	for _, err := range se {
+		// basically calling the Error method on every single error
+		// and putting it all in a string slice
+		s = append(s, err.Error())
+	}
+	return strings.Join(s, ", ")
 }
 
-// We use a pointertype here because we want to actually modify
-// the value of the given safeWriter.err value
-func (sw *safeWriter) writeln(s string) {
-	if sw.err != nil {
-		return
+const rows, columns = 9, 9
+
+// We define the length of the two dimensional array, so that
+// we actually create an array and no slice
+type Grid [rows][columns]int8
+
+// It is considered good practice to return the error interface
+// and not specific types like SudokuError, so that the method
+// stays flexible.
+func (g *Grid) Set(row, column int, digit int8) error {
+	var errs SudokuError
+	if !inBounds(row, column) {
+		errs = append(errs, ErrBounds)
 	}
-	_, sw.err = fmt.Fprintln(sw.w, s)
+	if !validDigit(digit) {
+		errs = append(errs, ErrDigit)
+	}
+	if len(errs) > 0 {
+		return errs
+	}
+	g[row][column] = digit
+	return nil
 }
 
-func proverbs(name string) error {
-	f, err := os.Create(name)
-	if err != nil {
-		return err
+func validDigit(digit int8) bool {
+	return digit >= 1 && digit <= 9
+}
+
+func inBounds(row, column int) bool {
+	if row < 0 || row >= rows {
+		return false
 	}
-	defer f.Close()
-
-	sw := safeWriter{w: f}
-	sw.writeln("Errors are values.")
-	sw.writeln("Don't just check errors, handle them gracefully.")
-	sw.writeln("Don't panic.")
-	sw.writeln("Make the zero value useful.")
-	sw.writeln("The bigger the interface, the weaker the abstraction.")
-	sw.writeln("interface{} says nothing.")
-	sw.writeln("Gofmt's style is no one's favorite, yet gofmt is everyone'sfavorite.")
-	sw.writeln("Documentation is for users.")
-	sw.writeln("A little copying is better than a little dependency.")
-	sw.writeln("Clear is better than clever.")
-	sw.writeln("Concurrency is not parallelism.")
-	sw.writeln("Don't communicate by sharing memory, share memory bycommunicating.")
-	sw.writeln("Channels orchestrate; mutexes serialize.")
-
-	return sw.err
+	if column < 0 || column >= columns {
+		return false
+	}
+	return true
 }
 
 func main() {
-
-	files, err := ioutil.ReadDir(".")
+	var g Grid
+	err := g.Set(10, 0, 15)
 	if err != nil {
-		fmt.Println(err)
+		// This could also be written as:
+		// if err, ok := err.(SudokuError); ok {}
+		// But i prefer this, because i think it is more concise with
+		// the rest of the syntax in go and looks even more readable to me
+		// The err.(SudokuError) is a type assertion, that checks if the given
+		// Error is of type SudokuError, if so err will get the type SudokuError
+		// and ok will be set to true.
+		// We need to do this, so that we can correctly handle the error output.
+		// Because SudokuError is of type []error, we have to destructure the slice
+		// in a for loop to access every actual error plus type and its message.
+		err, ok := err.(SudokuError)
+		if ok {
+			fmt.Printf("%d error(s) occured:\n", len(err))
+			for _, e := range err {
+				fmt.Printf("- %v", e)
+			}
+		}
+		// Will kill the current process.
 		os.Exit(1)
-	}
-
-	for _, file := range files {
-		fmt.Println(file.Name())
-	}
-
-	err = proverbs("proverbs.txt")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	files, err = ioutil.ReadDir(".")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	for _, file := range files {
-		fmt.Println(file.Name())
 	}
 }
